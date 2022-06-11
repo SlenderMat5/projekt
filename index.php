7ddb43883b500fec0509abb782f58e6a2ce7b019
Matej Petrlic <petrlic.matej@gmail.com>
2022-06-11 15:16:37 +0200

    comit on main

--- index.php
+++ index.php
@@ -4,11 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>0246098811</title>
 </head>
 <body>
     <?php
-    include nav.php
+    include 'nav.php';
     ?>
 </body>
 </html>
---
+++ nav.php
@@ -0,0 +1,10 @@
+<header>
+        <h1>Naslov</h1>
+        <nav>
+            <ul>
+                <li><a href="#">Link</a></li>
+                <li><a href="#">Link</a></li>
+                <li><a href="#">Link</a></li>
+            </ul>
+        </nav>
+    </header>
