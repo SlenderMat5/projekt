31e8dd553dc2f563d7bd3149c8dbe3c7438b8cdd
Matej Petrlic <petrlic.matej@gmail.com>
2022-06-12 13:29:04 +0200

    nesto

--- nav.php
+++ nav.php
@@ -1,10 +1,13 @@
 <header>
-        <h1>Naslov</h1>
+        <h1>L'OBS</h1>
         <nav>
             <ul>
-                <li><a href="#">Link</a></li>
-                <li><a href="#">Link</a></li>
-                <li><a href="#">Link</a></li>
+                <li><a href="index.php">HOME</a></li>
+                <li><a href="index.php#politique">POLITIQUE</a></li>
+                <li><a href="index.php#immobilier">IMMOBILIER</a></li>
+                <li><a href="administracija.html">ADMINISTRACIJA</a></li>
             </ul>
         </nav>
     </header>
+
+ 
