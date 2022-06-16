ebb193b597b5ec5b88e00f215aeb6e623d8e289a
Matej Petrlic <petrlic.matej@gmail.com>
2022-06-16 20:41:18 +0200

    binbo baggins

--- racun.php
+++ racun.php
@@ -9,7 +9,32 @@ die('Error connecting to MySQL server.'. mysqli_connect_error());
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title><?php echo $_SESSION['username'];?></title>
+    <style>
+    .grid-container {
+    display: grid;
+    grid-template-areas: 
+    'left middle middle middle middle middle' 
+    grid-column-gap: 10px;
+    } 
+
+    aside {
+    grid-area: left;
+    }
+
+    main {
+    grid-area: middle;
+    }
+
+    @media (max-width: 600px) {
+    .grid-container  {
+        grid-template-areas: 
+        'middle middle middle middle middle middle' 
+        'left left left left left left' 
+    }
+    }
+    </style>
 </head>
 <body>
 <?php
@@ -20,9 +45,14 @@ die('Error connecting to MySQL server.'. mysqli_connect_error());
         Hello <?php echo $_SESSION['username'];?>
     </h2>
 </nav>
+<div class="grid-container">
 <main>
-
+ 
 </main>
+<aside>
+    
+</aside>
+</div>
 <?php
     include 'footer.php';
 ?>
