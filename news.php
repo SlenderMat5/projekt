2f0f9c773de0bd9e68c00e49daf67a12e625f3cc
Matej Petrlic <petrlic.matej@gmail.com>
2022-06-17 00:34:05 +0200

    biljka

--- news.php
+++ news.php
@@ -1,9 +1,17 @@
 <?php
 $dbc = mysqli_connect('localhost', 'root', '', 'projekt') or
 die('Error connecting to MySQL server.'. mysqli_connect_error());
-
+$result=mysqli_query("Select * from novosti where id_novosti=".$_GET['id']." ");
 ?>
-
-<?php
-echo $_GET['id'];
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $_GET['']; ?></title>
+</head>
+<body>
+    
+</body>
+</html>
